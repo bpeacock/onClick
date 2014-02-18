c4486ef8240f7dee68cc2037f76ad4cfcacb9d59
Brian Peacock <bpeacock@fastfig.com>
2014-02-17 22:20:48 -0500

    Initial functions

--- src/main.js
+++ src/main.js
@@ -1,7 +1,62 @@
-var $ = require('jquery');
+var $ = require('jquery'),
+    _ = require('underscore');
 
-var click = function(config) {
+var $document   = $(document),
+    events      = {};
 
+var getPos = function(e) {
+    if (e.pageX || e.pageY) {
+        return {
+            x: e.pageX,
+            y: e.pageY
+        };
+    }
+    else {
+        return {
+            x: e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
+            y: e.clientY + document.body.scrollTop + document.documentElement.scrollTop
+        };
+    }
 };
 
+var click = function(events) {
+    var self = this;
+
+    _.each(events, function(callback, selector) {
+        if(isTouch) {
+            $document.on('touchstart', selector, function(e) {
+                var startTarget = e.target,
+                    startTime   = new date().getTime(),
+                    startPos    = getPos(e);
+
+                $document.one('touchend', selector, function(e) {
+                    e.preventDefault(); //Prevents click event from firing
+
+                    var time        = new date().getTime() - startTime,
+                        endPos      = getPos(e),
+                        distance    = Math.sqrt(
+                            Math.pow(endPos.x - startPos.x, 2) +
+                            Math.pow(endPos.y - startPos.y, 2)
+                        );
+
+                    if(time < self.timeLimit && distance < self.distanceLimit && startTarget === e.target) {
+                        callback(e);
+                    }
+                });
+            });
+        }
+
+        $document.on('click', selector, callback);
+    });
+};
+
+/*** Configuration Options ***/
+click.distanceLimit = 10;
+click.timeLimit     = 150;
+
+/*** Useful Properties ***/
+click.isTouch = ('ontouchstart' in window) ||
+                window.DocumentTouch && document instanceof DocumentTouch;
+
 module.exports = click;
+
